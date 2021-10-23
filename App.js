1fb22047cf0be2f86b9c14b2aeed524ed8c0a1ed
Jatinjain07Dazai <72065165+Jatinjain07Dazai@users.noreply.github.com>
2021-10-23 09:31:12 +0530

    Navigator set up in main app

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Index from './index';
 import Home from './home';
 import Closelist from './closelist';
+import Navigator from './Nav';
 export default function App() {
 return (
     //<Index/>
     //<Home/>
-    <Closelist/>
+    //<Closelist/>
+    <Navigator/>
   );
 }
 
